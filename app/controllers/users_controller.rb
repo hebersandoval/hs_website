5fcf26cfd7058a712530e0d0d2d377d8059bcd58
Heber Sandoval <he3ers@gmail.com>
2017-05-09 16:10:53 -0400

    User show page displays basic information of current logged in user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    unless current_user.admin?
+      unless @user == current_user
+        redirect_to root_path, :alert => "Access denied!"
+      end
+    end
+  end
 end
