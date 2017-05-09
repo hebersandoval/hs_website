b21dd23da7c2c3117a5f7da5fa11fac06c5ae6af
Heber Sandoval <he3ers@gmail.com>
2017-05-09 17:20:13 -0400

    A post can be created by a signed in user and only an admin can delete the post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Your post was successfully created!"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :user_id)
   end
 end
