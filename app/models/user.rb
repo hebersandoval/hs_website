89b13e3be015339b3c90f93a7a90c4459cfdd5b8
Heber Sandoval <he3ers@gmail.com>
2017-05-09 14:42:09 -0400

    Create role for user model with validation and set a callback method for default user role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  enum role: [:guest, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+  validates :name, presence: true
+
+  def set_default_role
+    self.role ||= :guest
+  end
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
