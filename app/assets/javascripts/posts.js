2feaf390c6f17cc768ae24f7fc42df7f22537370
Heber Sandoval <he3ers@gmail.com>
2017-05-11 15:29:13 -0400

    First attempt in making pagination with ajax>

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -43,3 +43,14 @@ $(function() {
     });
   });
 });
+
+// Pagination
+// $(function() {
+//   $(document.body).off('click', 'pagination a');
+//   $(document.body).on('click', 'pagination a', function(e) {
+//     e.preventDefault();
+//     var loadingHTML = "<div class='loading'>Loading...</div>";
+//     $("#blog_pagination").html(loadingHTML).load($(this).attr("href"));
+//     return false;
+//   });
+// });
