19fe4576235220714440d47fd4370743ff6ae3fc
Heber Sandoval <he3ers@gmail.com>
2017-05-11 08:12:18 -0400

    Added has_many through: post_categories to the post and category models to bind the two together.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
 
   belongs_to :user
+  has_many :post_categories
   has_many :categories, through: :post_categories
 
   def self.search(search)
