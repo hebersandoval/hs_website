a28fa393e325ae80796c9726580549813b9f1317
Heber Sandoval <he3ers@gmail.com>
2017-05-11 07:57:12 -0400

    Added has_many through and belongs_to association to category and post models along with validation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
 
   belongs_to :user
+  has_many :categories, through: :post_categories
 
   def self.search(search)
     where("title || content LIKE ?", "%" + search + "%")
