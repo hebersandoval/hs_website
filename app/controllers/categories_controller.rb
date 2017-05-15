b189215b5d4542e80d6cbdcf662507011e48b253
Heber Sandoval <he3ers@gmail.com>
2017-05-15 19:12:46 -0400

    Can edit a category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,10 +10,24 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.create(category_params)
     if @category.save
-      flash[:notice] = "Successfully created category"
+      flash[:notice] = "Successfully created category!"
       redirect_to categories_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      flash[:notice] = "Successfully updated category!"
+      redirect_to categories_path
+    else
+      render :edit
     end
   end
 
