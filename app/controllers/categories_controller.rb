d3dbcd2bcef6f20e71fe1f36ae1320b314cf3be6
Heber Sandoval <he3ers@gmail.com>
2017-05-15 18:22:10 -0400

    Create action on categories controller makes a new category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,5 +7,19 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      flash[:notice] = "Successfully created category"
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
