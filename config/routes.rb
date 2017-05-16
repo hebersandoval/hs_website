e0b0e2153f21c2e8d5457722306c628b5329b14b
Heber Sandoval <he3ers@gmail.com>
2017-05-16 10:02:35 -0400

    Commen model added as a nested resource of post.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:create]
+  end
   resources :categories
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511114328) do
+ActiveRecord::Schema.define(version: 20170516135805) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "post_categories", force: :cascade do |t|
