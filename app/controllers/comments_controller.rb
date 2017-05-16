956d1b4d446630b348dc90a1792bb488158f12a0
Heber Sandoval <he3ers@gmail.com>
2017-05-16 11:51:27 -0400

    Comment can be created and displayed in the post show page with the user's info.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,20 @@
 class CommentsController < ApplicationController
-  def new
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:notice] = "Successfully created comment!"
+      # redirect_to post_comments_path(@comment)
+      redirect_to @comment.post
+    else
+      redirect_to :back, alert: "Your comment was not saved!"
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
