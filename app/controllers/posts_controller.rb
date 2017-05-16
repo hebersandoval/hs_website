6974f26932cc468ba7d054d4b04c65386a7ea882
Heber Sandoval <he3ers@gmail.com>
2017-05-16 11:41:20 -0400

    Add a new form for comments in the post show page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html { render :show }
