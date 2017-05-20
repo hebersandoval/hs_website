de5a51a3eeae7e1ed29f966b656930566e9405de
Heber Sandoval <he3ers@gmail.com>
2017-05-20 18:26:35 -0400

    Add resources for project and tag.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users
+  
   resources :posts do
     resources :comments, only: [:create]
   end
+
   resources :categories
+  resources :projects
+  resources :tags
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
