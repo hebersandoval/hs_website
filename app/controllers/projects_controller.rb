7ac9bda8b7d998a0878a0224f2d6128ad5b0e6d1
Heber Sandoval <he3ers@gmail.com>
2017-05-20 18:58:03 -0400

    A project with its related tags can be created through the projects controller create action.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,25 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.create(project_params)
+    # Add tag to project
+    if params[:tag_id]
+      @tag = Tag.find(params[:tag_id])
+      @project.tags << @tag
+    end
+    if @project.save
+      flash[:notice] = "Successfully created project!"
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def project_params
+    params.require(:project).permit(:title, :description, tag_ids: [])
+  end
 end
