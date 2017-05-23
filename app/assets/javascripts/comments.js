b7ad302dd9d4d3b752862036dea4d4d5e970cf4b
Heber Sandoval <he3ers@gmail.com>
2017-05-23 18:18:53 -0400

    A comment can be added via ajax and displayed in the post show page without full page refresh.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -14,12 +14,23 @@ Comment.prototype.renderComment = function() {
   return Comment.template(this);
 }
 
+Comment.success = function(json) {
+  // i want json to be a js obj of the comment that was created
+  var comment = new Comment(json)
+  var displayComment = comment.renderComment();
+  $("#comment_content").val("")
+  $(".comment").append(displayComment);
+}
+
+Comment.error = function(resp) {
+  console.log('Did it break?', resp);
+}
+
 // Submit comment form using ajax
 $(function() {
   // rails will generate the '#new_comment' in the html markup
   $("form#new_comment").on("submit", function(e) {
     e.preventDefault();
-    console.log("stop")
     var $form = $(this);
     var action = $form.attr("action");
     var params = $form.serialize();
@@ -30,18 +41,7 @@ $(function() {
       dataType: "json",
       method: "POST"
     })
-      .success(function(json) {
-        // i want json to be a js obj of the comment that was created
-        console.log(json)
-
-        var comment = new Comment(json)
-        console.log(comment)
-        var displayComment = comment.renderComment();
-        console.log(displayComment)
-        $("comment").append(displayComment);
-      })
-      .error(function(resp) {
-        console.log('You broke it?', resp)
-      })
+      .success(Comment.success)
+      .error(Comment.error)
   });
 });
