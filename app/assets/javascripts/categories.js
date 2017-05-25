082ed47d37447eaf985609e581f40682e986eeda
Heber Sandoval <he3ers@gmail.com>
2017-05-25 13:59:28 -0400

    Working on making categories resource a full ajax version.

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -29,63 +29,63 @@
 //   }
 // }
 
-$(document).ready(function() {
-  compileIndexCategoryTemplate();
-  indexCategories();
-  compileNewCategoryTemplate();
-  newCategory();
-});
-
-function Category(attributes) {
-  this.id = attributes.id;
-  this.name = attributes.name;
-}
-
-// // Render handlebars template
-Category.prototype.renderIndexCategory = function() {
-  return indexCategoryTemplate({id: this.id, name: this.name});
-}
-
-Category.prototype.renderNewCategory = function() {
-  return newCategoryTemplate({id: this.id, name: this.name});
-}
-
-// Compile stuff
-function compileIndexCategoryTemplate() {
-  var indexCategorySource = $("#indexCategoryTemplate").html();
-  if (indexCategorySource !== undefined) {
-    indexCategoryTemplate = Handlebars.compile(indexCategorySource);
-  }
-}
-
-function compileNewCategoryTemplate(){
- newCategorySource = $("#newCategoryTemplate").html();
- if ( newCategorySource !== undefined ) {
-  newCategoryTemplate = Handlebars.compile(newCategorySource);
- }
-}
-
-// Get all categories via AJAX GET request
-function indexCategories() {
-  var url = window.location.pathname;
-  $.get(url, function(data) {
-    $.each(data, function(index, category) {
-      var indexCategory = new Category(category);
-      var indexCategoryRender = indexCategory.renderIndexCategory();
-      $(".categories-block").prepend(indexCategoryRender);
-    })
-  }, "json");
-}
-
-// Get the new note form via AJAX GET request
-function newCategory() {
- $(document).on("click", '.js-new-category', function(event) {
-  event.preventDefault();
-  $.get( $(event.target).attr('href'), function( data ) {
-   var category = new Category(data);
-   var categoryRender = category.renderNewCategory();
-   $("#new-category").html("");
-   $("#new-category").append(categoryRender);
-  });
- });
-}
+// $(document).ready(function() {
+//   compileIndexCategoryTemplate();
+//   indexCategories();
+//   compileNewCategoryTemplate();
+//   newCategory();
+// });
+//
+// function Category(attributes) {
+//   this.id = attributes.id;
+//   this.name = attributes.name;
+// }
+//
+// // // Render handlebars template
+// Category.prototype.renderIndexCategory = function() {
+//   return indexCategoryTemplate({id: this.id, name: this.name});
+// }
+//
+// Category.prototype.renderNewCategory = function() {
+//   return newCategoryTemplate({id: this.id, name: this.name});
+// }
+//
+// // Compile stuff
+// function compileIndexCategoryTemplate() {
+//   var indexCategorySource = $("#indexCategoryTemplate").html();
+//   if (indexCategorySource !== undefined) {
+//     indexCategoryTemplate = Handlebars.compile(indexCategorySource);
+//   }
+// }
+//
+// function compileNewCategoryTemplate(){
+//  newCategorySource = $("#newCategoryTemplate").html();
+//  if ( newCategorySource !== undefined ) {
+//   newCategoryTemplate = Handlebars.compile(newCategorySource);
+//  }
+// }
+//
+// // Get all categories via AJAX GET request
+// function indexCategories() {
+//   var url = window.location.pathname;
+//   $.get(url, function(data) {
+//     $.each(data, function(index, category) {
+//       var indexCategory = new Category(category);
+//       var indexCategoryRender = indexCategory.renderIndexCategory();
+//       $(".categories-block").prepend(indexCategoryRender);
+//     })
+//   }, "json");
+// }
+//
+// // Get the new category form via AJAX GET request
+// function newCategory() {
+//  $(document).on("click", '.js-new-category', function(event) {
+//   event.preventDefault();
+//   $.get( $(event.target).attr('href'), function( data ) {
+//    var category = new Category(data);
+//    var categoryRender = category.renderNewCategory();
+//    $("#new-category").html("");
+//    $("#new-category").append(categoryRender);
+//   });
+//  });
+// }
